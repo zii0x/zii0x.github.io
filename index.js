131464924a61ceb1b43586b57d5c1b4a3baf8a17
RandQuran <siddiqmutarji.m@gmail.com>
2023-10-11 00:30:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import '@material/web/button/filled-button.js';
 import '@material/web/button/outlined-button.js';
 import '@material/web/checkbox/checkbox.js';
+import '@material/web/ripple/ripple.js';
